03e5e7aedb311ef6bed54bd3153b984f515ee50c
guin-D <ntiendung0310@gmail.com>
2025-10-31 11:35:15 +0700

    add special input cases

--- srcs/FP8_multiplier.v
+++ srcs/FP8_multiplier.v
@@ -1,7 +1,7 @@
 module FP8_multiplier (
     input  [7:0] a,
     input  [7:0] b,
-    output [7:0] product
+    output reg [7:0] product
 );
 
     wire       sign_a, sign_b, sign_p;
@@ -12,6 +12,11 @@ module FP8_multiplier (
     wire [3:0] mul_a, mul_b;
     wire [7:0] mul_product;
     wire       comp;
+    
+    wire       is_a_nan, is_b_nan;
+    wire       is_a_inf, is_b_inf;
+    wire       is_a_zer, is_b_zer;
+    wire       is_p_nan, is_p_inf, is_p_zer;
     
     assign sign_a = a[7];
     assign sign_b = b[7];
@@ -31,6 +36,21 @@ module FP8_multiplier (
         .product(mul_product)
     );
     
+    assign is_a_nan = (exponent_a == 4'b1111) && (mantissa_a != 3'b000);
+    assign is_b_nan = (exponent_b == 4'b1111) && (mantissa_b != 3'b000);
+    
+    assign is_a_inf = (exponent_a == 4'b1111) && (mantissa_a == 3'b000);
+    assign is_b_inf = (exponent_b == 4'b1111) && (mantissa_b == 3'b000);
+    
+    assign is_a_zer = (exponent_a == 4'b0000) && (mantissa_a == 3'b000);
+    assign is_a_zer = (exponent_a == 4'b0000) && (mantissa_a == 3'b000);
+    
+    assign is_p_nan = (is_a_nan || is_b_nan) 
+                      || (is_a_inf && is_b_zer)
+                      || (is_a_zer && is_b_inf);
+    assign is_p_inf = is_a_inf || is_b_inf;
+    assign is_p_zer = is_a_zer || is_b_zer;
+    
     assign comp = (mul_product[7] == 1'b1) ? 1 : 0;
     
     assign sign_p = sign_a ^ sign_b;
@@ -38,6 +58,16 @@ module FP8_multiplier (
     assign mantissa_p = (mul_product == 0) ? 0 :
                         (mul_product[7] == 1'b1) ? mul_product[6:4] : mul_product[5:3];
 
-    assign product = {sign_p, exponent_p, mantissa_p};
+    always @*
+    begin
+        if (is_p_nan) 
+            product <= 8'bzzzzzzzz;
+        else if (is_p_inf)
+            product <= {sign_p, 7'b1111000};
+        else if (is_p_zer)
+            product <= {sign_p, 7'b0000000};
+        else 
+            product <= {sign_p, exponent_p, mantissa_p};
+    end
     
 endmodule
